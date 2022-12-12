1f7c40a319f8e063ec3800830c898fc4c24d5a46
ericlee830308 <120347874+ericlee830308@users.noreply.github.com>
2022-12-12 10:47:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Hello World!')
+
+streamlit.header('Hello World!')
+streamlit.text('Hello World!1')
+streamlit.text('Hello World!2')
+streamlit.text('Hello World!3')
+streamli.text('Hello World!4')
