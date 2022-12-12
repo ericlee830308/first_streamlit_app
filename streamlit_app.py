79031c3b46a2c9d73e3986c2d0f9690311b93f83
ericlee830308 <120347874+ericlee830308@users.noreply.github.com>
2022-12-12 10:50:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('Hello World!2')
 streamlit.text('Hello World!3')
 streamlit.text('Hello World!4')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(fruit_list)
